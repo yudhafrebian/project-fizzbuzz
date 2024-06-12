6ad73a30d8edafdaec0e8b500aa59d694ffa9755
yudhafrebian <anandayudha777@gmail.com>
2024-06-12 13:37:15 +0700

    Complete feature for Javascript and CSS

---
+++ script.js
@@ -0,0 +1,35 @@
+const fizzBuzz = (number1, number2) => {
+    
+    const result = number1 + number2;
+    if (result % 3 === 0 && result % 5 === 0) {
+        return `FizzBuzz ${number1} + ${number2} = ${result}`;
+    }
+    else if (result % 3 === 0) {
+        return `Fizz ${number1} + ${number2} = ${result}`;
+    }
+    else if (result % 5 === 0) {
+        return `Buzz ${number1} + ${number2} = ${result}`;
+    }
+    else {
+        return `${number1} + ${number2} = ${result}`;
+    }
+}
+
+function getValue() {
+    const input1 = parseInt(document.getElementById('number1').value);
+    const input2 = parseInt(document.getElementById('number2').value);
+    
+    if (isNaN(input1)) {
+        alert(`Input abc is not a number`);
+        return;
+    } else if (isNaN(input2)) {
+        alert(`Input abc is not a number`);
+        return;
+    }
+    
+    const result = fizzBuzz(input1, input2);
+    const divResult = document.getElementById("result");
+    const paragraph = document.createElement("p");
+    paragraph.innerHTML = result ;
+    divResult.append(paragraph);
+}
