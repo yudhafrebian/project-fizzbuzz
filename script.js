8151f549bd192b5fbe5fe378d6b925dcc4158f81
yudhafrebian <anandayudha777@gmail.com>
2024-06-19 20:24:31 +0700

    finish project

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const fizzBuzz = (number1, number2) => {
-    
     const result = number1 + number2;
-    if (number1 + number2 === 0){
-        alert (`Input must not 0`)
-        return;
+    if (number1 === 0 || number2 === 0) {
+        alert('input must not be 0');
+        return null;
     }
     if (result % 3 === 0 && result % 5 === 0) {
         return `FizzBuzz ${number1} + ${number2} = ${result}`;
@@ -14,26 +13,29 @@ const fizzBuzz = (number1, number2) => {
     if (result % 5 === 0) {
         return `Buzz ${number1} + ${number2} = ${result}`;
     }
-    else {
-        return `${number1} + ${number2} = ${result}`;
-    }
-}
+    return `${number1} + ${number2} = ${result}`;
+};
 
 function getValue() {
-    const input1 = parseInt(document.getElementById('number1').value);
-    const input2 = parseInt(document.getElementById('number2').value);
-    
+    const value1 = document.getElementById('number1').value;
+    const value2 = document.getElementById('number2').value;
+
+    const input1 = parseInt(value1);
+    const input2 = parseInt(value2);
+
     if (isNaN(input1)) {
-        alert(`Input abc is not a number`);
+        alert(`Input ${value1} is not a number`);
         return;
     } else if (isNaN(input2)) {
-        alert(`Input abc is not a number`);
+        alert(`Input ${value2} is not a number`);
         return;
     }
-    
+
     const result = fizzBuzz(input1, input2);
-    const divResult = document.getElementById("result");
-    const paragraph = document.createElement("p");
-    paragraph.innerHTML = result ;
-    divResult.append(paragraph);
+    if (result !== null) {
+        const divResult = document.getElementById('result');
+        const paragraph = document.createElement('p');
+        paragraph.innerHTML = result;
+        divResult.append(paragraph);
+    }
 }
